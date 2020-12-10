322c305599ce899a4444e03ef49e455064dd4600
josh-spratt <61611179+josh-spratt@users.noreply.github.com>
2020-12-09 23:15:52 -0600

    over 80% through week 2 stepik material

---
+++ week_2/hamming_distance.py
@@ -0,0 +1,21 @@
+def ApproximatePatternMatching(Text, Pattern, d):
+    positions = [] # initializing list of positions
+    for i in range(len(Text)-len(Pattern)+1):
+        if HammingDistance(Text[i:i+len(Pattern)], Pattern) <= d:
+            positions.append(i)
+    return positions
+
+
+def HammingDistance(p, q):
+    count = 0
+    for i in range(len(p)):
+        if p[i] != q[i]:
+            count += 1
+    return count
+
+
+Text = 'CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGTCAATCAAAT'
+Pattern = 'ATTCTGGA'
+d = 3
+ApproximatePatternMatching = ApproximatePatternMatching(Text, Pattern, d)
+print(ApproximatePatternMatching)
