05a2096a5969df000a4a720f132aedfa4f4198f5
josh-spratt <61611179+josh-spratt@users.noreply.github.com>
2020-12-14 23:55:19 -0600

    done with section 3 in week 3

--- week_3/scoring_motifs.py
+++ week_3/scoring_motifs.py
@@ -1,3 +1,5 @@
+# Input:  A set of kmers Motifs
+# Output: Count(Motifs)
 def Count(Motifs):
     count = {}
     k = len(Motifs[0])
@@ -13,6 +15,63 @@ def Count(Motifs):
     return count
 
 
+# Input:  A list of kmers Motifs
+# Output: the profile matrix of Motifs, as a dictionary of lists.
+def Profile(Motifs):
+    profile = Count(Motifs)
+    t = len(Motifs)
+    k = len(Motifs[0])
+    for i in profile.keys():
+        for j in range(k):
+            profile[i][j] /= t
+    return profile
+
+
+# Input:  A set of kmers Motifs
+# Output: A consensus string of Motifs.
+def Count(Motifs):
+    count = {}
+    k = len(Motifs[0])
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+            count[symbol].append(0)
+    t = len(Motifs)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+    return count
+
+
+# Input:  A set of kmers Motifs
+# Output: A consensus string of Motifs.
+def Consensus(Motifs):
+    k = len(Motifs[0])
+    count = Count(Motifs)
+    consensus = ""
+    for j in range(k):
+        m = 0
+        frequentSymbol = ""
+        for symbol in "ACGT":
+            if count[symbol][j] > m:
+                m = count[symbol][j]
+                frequentSymbol = symbol
+        consensus += frequentSymbol
+    return consensus
+
+
+# Input:  A set of k-mers Motifs
+# Output: The score of these k-mers.
+def Score(Motifs):
+    consensus = Consensus(Motifs)
+    count = 0
+    for motif in Motifs:
+        for index in range(len(motif)):
+            if motif[index] != consensus[index]:
+                count += 1
+    return count
+
+
 motif_input = ["AACGTA", "CCCGTT", "CACCTT", "GGATTA", "TTCCGG"]
-motif_count_function_res = Count(motif_input)
-print(motif_count_function_res)
+print(Score(motif_input))
