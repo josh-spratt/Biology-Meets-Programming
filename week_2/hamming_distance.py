31b257f036e3224b26b6027ed5c25d447036e7fd
josh-spratt <61611179+josh-spratt@users.noreply.github.com>
2020-12-14 21:27:52 -0600

    week 3 section 1.3 6/14 steps passed

--- week_2/hamming_distance.py
+++ week_2/hamming_distance.py
@@ -1,5 +1,5 @@
 def ApproximatePatternMatching(Text, Pattern, d):
-    positions = [] # initializing list of positions
+    positions = []  # initializing list of positions
     for i in range(len(Text)-len(Pattern)+1):
         if HammingDistance(Text[i:i+len(Pattern)], Pattern) <= d:
             positions.append(i)
@@ -18,8 +18,4 @@ Text = 'CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGTCAATCAAAT'
 Pattern = 'ATTCTGGA'
 d = 3
 #ApproximatePatternMatching = ApproximatePatternMatching(Text, Pattern, d)
-#print(ApproximatePatternMatching)
-p = 'CTTGAAGTGGACCTCTAGTTCCTCTACAAAGAACAGGTTGACCTGTCGCGAAG'
-q = 'ATGCCTTACCTAGATGCAATGACGGACGTATTCCTTTTGCCTCAACGGCTCCT'
-HammingDistance = HammingDistance(p,q)
-print(HammingDistance)
+# print(ApproximatePatternMatching)
