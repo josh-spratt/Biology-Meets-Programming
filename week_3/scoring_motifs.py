caf4b45086f752a1ede407c8f8221da08a3b197e
josh-spratt <61611179+josh-spratt@users.noreply.github.com>
2020-12-22 15:45:05 -0600

    week 3 complete

--- week_3/scoring_motifs.py
+++ week_3/scoring_motifs.py
@@ -29,23 +29,6 @@ def Profile(Motifs):
 
 # Input:  A set of kmers Motifs
 # Output: A consensus string of Motifs.
-def Count(Motifs):
-    count = {}
-    k = len(Motifs[0])
-    for symbol in "ACGT":
-        count[symbol] = []
-        for j in range(k):
-            count[symbol].append(0)
-    t = len(Motifs)
-    for i in range(t):
-        for j in range(k):
-            symbol = Motifs[i][j]
-            count[symbol][j] += 1
-    return count
-
-
-# Input:  A set of kmers Motifs
-# Output: A consensus string of Motifs.
 def Consensus(Motifs):
     k = len(Motifs[0])
     count = Count(Motifs)
@@ -73,5 +56,47 @@ def Score(Motifs):
     return count
 
 
-motif_input = ["AACGTA", "CCCGTT", "CACCTT", "GGATTA", "TTCCGG"]
-print(Score(motif_input))
+# Input:  String Text and profile matrix Profile
+# Output: Pr(Text, Profile)
+def Pr(Text, Profile):
+    p = 1
+    for i in range(len(Text)):
+        p = p*Profile[Text[i]][i]
+    return p
+
+
+def ProfileMostProbableKmer(Text, k, Profile):
+    max_prob = 0
+    most_probable = Text[:k]
+    n = len(Text)
+    for i in range(n - k + 1):
+        kmer = Text[i: i + k]
+        probability = Pr(kmer, Profile)
+        if probability > max_prob:
+            max_prob = probability
+            most_probable = kmer
+    return most_probable
+
+
+# Input:  A list of kmers Dna, and integers k and t (where t is the number of kmers in Dna)
+# Output: GreedyMotifSearch(Dna, k, t)
+def GreedyMotifSearch(Dna, k, t):
+    BestMotifs = []
+    for i in range(0, t):
+        BestMotifs.append(Dna[i][0:k])
+    n = len(Dna[0])
+    for i in range(n-k+1):
+        Motifs = []
+        Motifs.append(Dna[0][i:i+k])
+        for j in range(1, t):
+            P = Profile(Motifs[0:j])
+            Motifs.append(ProfileMostProbableKmer(Dna[j], k, P))
+        if Score(Motifs) < Score(BestMotifs):
+            BestMotifs = Motifs
+    return BestMotifs
+
+
+profile = {'A': [0.4,  0.3,  0.0,  0.1,  0.0,  0.9], 'C': [0.2,  0.3,  0.0,  0.4,  0.0,  0.1], 'G': [
+    0.1,  0.3,  1.0,  0.1,  0.5,  0.0], 'T': [0.3,  0.1,  0.0,  0.4,  0.5,  0.0]}
+answer = Pr('AAGTTC', profile)
+print(answer)
